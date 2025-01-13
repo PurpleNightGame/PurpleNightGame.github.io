db7a7555766c535c169fa525f74e2fe9c90c1c9a
NDYblog <fish20071031@outlook.com>
2025-01-13 13:57:23 +0800

    更新仓库配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' 
-    ? '/member-management-system/' 
-    : '/',
+  base: '/',
   plugins: [vue()],
   server: {
     port: 8080,
